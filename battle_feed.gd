a87cf25857284b8420acdf1d4589240edb63f261
ZetCipher <numbah90@gmail.com>
2023-07-07 16:27:34 -0400

    battle feed

---
+++ battle_feed.gd
@@ -0,0 +1,18 @@
+extends Control
+
+var lines : Array[String] = ["", "", "", ""]
+
+func _ready():
+	pass
+
+func clear_lines():
+	lines = ["", "", "", ""]
+	update_text()
+
+func add_line(text : String):
+	lines.pop_front()
+	lines.append(text)
+	update_text()
+
+func update_text():
+	$RichTextLabel.text = lines[0] + "\n" + lines[1] + "\n" + lines[2] + "\n" + lines[3]
