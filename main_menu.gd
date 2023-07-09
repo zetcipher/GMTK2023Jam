0cbc25425a1b51a07a2f67651b1a194dc3f764f5
ZetCipher <numbah90@gmail.com>
2023-07-09 00:23:07 -0400

    main menu scene

---
+++ main_menu.gd
@@ -0,0 +1,18 @@
+extends Control
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	get_window().size = Vector2i(640,360) * 3
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		if $Title.visible:
+			$Title.hide()
+			$Info.show()
+		else:
+			get_tree().change_scene_to_file("res://battle_scene.tscn")
+	if Input.is_action_just_pressed("ui_cancel"):
+		$Title.show()
+		$Info.hide()
